4b8a31ec2c11c44b26bb38b77a04fc8a78da2c21
robinsph21 <robinsph21@up.edu>
2020-09-23 02:19:19 -0700

    Add basic homescreen
    Overwrites the HomeStackScreen.jsx. Will possibly move to Home.jsx to match the organization that has been set up. Also need to implement a function to create the cards instead of explicit definitions.

--- src/components/HomeStackScreen.jsx
+++ src/components/HomeStackScreen.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Button, Text, StyleSheet } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { Button, ScrollView, Text, View, StyleSheet, Image } from 'react-native';
 import { Icon } from 'react-native-elements';
+import { createStackNavigator } from '@react-navigation/stack';
 import screens from '../constants/screens';
 import theme from '../constants/theme';
+import Humpback from '../../assets/humpback.jpg';
+import Hummingbird from '../../assets/hummingbird.jpg';
+import RedPanda from '../../assets/redPanda.jpg';
+import Octopus from '../../assets/octopus.jpg';
+import WhaleShark from '../../assets/whaleshark.jpg';
+import ForestLizard from '../../assets/lizard.jpg';
+import Elephant from '../../assets/elephant.jpg';
+import Jaguar from '../../assets/jaguar.jpg';
 
 const HomeStack = createStackNavigator();
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreenOld = ({ navigation }) => {
   return (
     <Button
       title="Switch to Login View"
@@ -15,6 +23,185 @@ const HomeScreen = ({ navigation }) => {
     />
   );
 };
+
+const HomeScreen = ({ nagivation }) => {
+  return (
+    <View>
+      {/* TODO: Turn from ScrollView into something FlatView*/}
+      <ScrollView contentContainerStyle={styles.content}>
+        <View style={styles.sortBy}>
+          <Text style={styles.fontLato}>
+            Last Added
+          </Text>
+          <Icon
+            name="arrowdown"
+            type="antdesign"
+            size={18}
+            color={theme.black}
+          />
+        </View>
+        <View style={styles.addNew}>
+          <Text style={styles.addNewText}>
+            + Add new sighting
+          </Text>
+        </View>
+        {/* TODO: Split into separate "SightingCards" function based creation*/}
+        <View style={styles.sightingCard}>
+          <Image style={styles.imageCover} source={Humpback} />
+          <View style={styles.sightingInfo}>
+            <View style={styles.sightingText}>
+              <Text style={styles.sightingTitle}>
+                Humpback Whale
+              </Text>
+              <Text style={styles.sightingDate}>
+                September 23rd, 2019
+              </Text>
+            </View>
+            <Icon
+              name="more-vert"
+              type="materialicons"
+              size={28}
+              color={theme.black}
+            />
+          </View>
+        </View>
+        <View style={styles.sightingCard}>
+          <Image style={styles.imageCover} source={Hummingbird} />
+          <View style={styles.sightingInfo}>
+            <View style={styles.sightingText}>
+              <Text style={styles.sightingTitle}>
+                Anna's Hummingbird
+              </Text>
+              <Text style={styles.sightingDate}>
+                September 23rd, 2019
+              </Text>
+            </View>
+            <Icon
+              name="more-vert"
+              type="materialicons"
+              size={32}
+              color={theme.black}
+            />
+          </View>
+        </View>
+        <View style={styles.sightingCard}>
+          <Image style={styles.imageCover} source={RedPanda} />
+          <View style={styles.sightingInfo}>
+            <View style={styles.sightingText}>
+              <Text style={styles.sightingTitle}>
+                Red Panda
+              </Text>
+              <Text style={styles.sightingDate}>
+                September 23rd, 2019
+              </Text>
+            </View>
+            <Icon
+              name="more-vert"
+              type="materialicons"
+              size={32}
+              color={theme.black}
+            />
+          </View>
+        </View>
+        <View style={styles.sightingCard}>
+          <Image style={styles.imageCover} source={Octopus} />
+          <View style={styles.sightingInfo}>
+            <View style={styles.sightingText}>
+              <Text style={styles.sightingTitle}>
+                Maldives Octopus
+              </Text>
+              <Text style={styles.sightingDate}>
+                September 23rd, 2019
+              </Text>
+            </View>
+            <Icon
+              name="more-vert"
+              type="materialicons"
+              size={32}
+              color={theme.black}
+            />
+          </View>
+        </View>
+        <View style={styles.sightingCard}>
+          <Image style={styles.imageCover} source={WhaleShark} />
+          <View style={styles.sightingInfo}>
+            <View style={styles.sightingText}>
+              <Text style={styles.sightingTitle}>
+                Whale Shark
+              </Text>
+              <Text style={styles.sightingDate}>
+                September 23rd, 2019
+              </Text>
+            </View>
+            <Icon
+              name="more-vert"
+              type="materialicons"
+              size={32}
+              color={theme.black}
+            />
+          </View>
+        </View>
+        <View style={styles.sightingCard}>
+          <Image style={styles.imageCover} source={ForestLizard} />
+          <View style={styles.sightingInfo}>
+            <View style={styles.sightingText}>
+              <Text style={styles.sightingTitle}>
+                Indonesian Forest Liza...
+              </Text>
+              <Text style={styles.sightingDate}>
+                September 23rd, 2019
+              </Text>
+            </View>
+            <Icon
+              name="more-vert"
+              type="materialicons"
+              size={32}
+              color={theme.black}
+            />
+          </View>
+        </View>
+        <View style={styles.sightingCard}>
+          <Image style={styles.imageCover} source={Elephant} />
+          <View style={styles.sightingInfo}>
+            <View style={styles.sightingText}>
+              <Text style={styles.sightingTitle}>
+                African Bush Elephant
+              </Text>
+              <Text style={styles.sightingDate}>
+                September 23rd, 2019
+              </Text>
+            </View>
+            <Icon
+              name="more-vert"
+              type="materialicons"
+              size={32}
+              color={theme.black}
+            />
+          </View>
+        </View>
+        <View style={styles.sightingCard}>
+          <Image style={styles.imageCover} source={Jaguar} />
+          <View style={styles.sightingInfo}>
+            <View style={styles.sightingText}>
+              <Text style={styles.sightingTitle}>
+                North American Jaguar
+              </Text>
+              <Text style={styles.sightingDate}>
+                September 23rd, 2019
+              </Text>
+            </View>
+            <Icon
+              name="more-vert"
+              type="materialicons"
+              size={32}
+              color={theme.black}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
 
 export default function HomeStackScreen({ navigation }) {
   return (
@@ -27,9 +214,18 @@ export default function HomeStackScreen({ navigation }) {
             type="font-awesome"
             color={theme.black}
             onPress={() => navigation.toggleDrawer()}
-            iconStyle={styles.icon}
+            iconStyle={styles.iconLeft}
           />
         ),
+        headerRight: () => (
+          <Icon
+            name="search"
+            type="font-awesome"
+            color={theme.black}
+            //onPress={() => navigation.toggleDrawer()} TODO: replace with search bar in the banner
+            iconStyle={styles.iconRight}
+          />
+        )
       }}
     >
       <HomeStack.Screen
@@ -48,7 +244,98 @@ const styles = StyleSheet.create({
     fontFamily: 'Lato-Regular',
     fontSize: 14,
   },
-  icon: {
+  iconLeft: {
     marginLeft: 16,
+  },
+  iconRight: {
+    marginRight: 16,
+  },
+  content: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    overflow: 'visible',
+    paddingBottom: 5,
+  },
+  sortBy: {
+    width: 102,
+    marginTop: 10,
+    marginHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignSelf: 'flex-start',
+  },
+  fontLato: {
+    fontFamily: 'Lato-Regular',
+    fontSize: 16,
+  },
+  addNew: {
+    marginVertical: 11,
+    marginVertical: 11,
+    width: '94%',
+    padding: 25,
+    justifyContent: 'center',
+    borderStyle: 'dashed',
+    borderWidth: 2,
+    borderColor: '#888',
+    borderRadius: 6,
+  },
+  addNewText: {
+    fontSize: 20,
+    fontFamily: 'Lato-Regular',
+    textAlign: 'center',
+    color: '#888',
+  },
+  sightingCard: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    marginVertical: 10,
+    width: '95%',
+    height: 80,
+    justifyContent: 'center',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: "#CCC",
+    borderRadius: 6,
+    // TODO: Relpace with react-native-shadow
+    // iOS
+    shadowColor: "#000",
+    shadowOffset: {
+    	width: 0,
+    	height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    // Android
+    elevation: 2,
+  },
+  sightingInfo: {
+    paddingLeft: 22,
+    paddingRight: 4,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flex: 2,
+    alignItems: 'center',
+  },
+  imageCover: {
+    resizeMode: 'cover', // Doesn't work?
+    borderTopLeftRadius: 6,
+    borderBottomLeftRadius: 6,
+    height: 78,
+    flex: 1,
+    overflow: 'hidden',
+    alignSelf: 'center',
+  },
+  sightingText: {
+    justifyContent: 'space-around',
+    height: 36,
+  },
+  sightingTitle: {
+    fontSize: 18,
+    fontFamily: 'Lato-Regular',
+  },
+  sightingDate: {
+    fontSize: 12,
+    fontFamily: 'Lato-Regular',
+    color: "#777",
   },
 });
---
+++ src/screens/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ActivityIndicator, StyleSheet, View, Image, Text } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeStackScreen from '../../components/HomeStackScreen';
+import theme from '../../constants/theme';
+
+export default function HomeScreen() {
+  return (
+    {/* TODO: Implement separate screen file? */}
+  );
+}
+
+const styles = StyleSheet.create({
+  {/* TODO: See above. Implement styles in an organized way */}
+});
