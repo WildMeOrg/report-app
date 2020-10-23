32252233f01af8929b7d72873432133d4e0bb5fa
degrood21 <35583946+degrood21@users.noreply.github.com>
2020-10-22 22:11:48 -0700

    Added Search Bar
    Clicking search bar icon reveals a text input where the name of the app used to be and replaces search icon with an 'x' for when the user wants to get out of search

--- src/components/HomeStackScreen.jsx
+++ src/components/HomeStackScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -6,45 +6,91 @@ import screens from '../constants/screens';
 import theme from '../constants/theme';
 import Typography from './Typography';
 import HomeScreen from '../screens/home/Home.jsx';
+import { TextInput } from 'react-native-gesture-handler';
+import globalStyles from '../styles/globalStyles';
 
 const HomeStack = createStackNavigator();
 
 export default function HomeStackScreen({ navigation }) {
-  return (
-    <HomeStack.Navigator
-      screenOptions={{
-        headerTitleAlign: 'center',
-        headerLeft: () => (
-          <Icon
-            name="bars"
-            type="font-awesome"
-            color={theme.black}
-            onPress={() => navigation.toggleDrawer()}
-            iconStyle={headerStyles.iconLeft}
-          />
-        ),
-        headerRight: () => (
-          <Icon
-            name="search"
-            type="font-awesome"
-            color={theme.black}
-            //onPress={() => navigation.toggleDrawer()} TODO: replace with search bar in the banner
-            iconStyle={headerStyles.iconRight}
-          />
-        ),
-      }}
-    >
-      <HomeStack.Screen
-        name={screens.home}
-        component={HomeScreen}
-        options={{
-          headerTitle: () => (
-            <Typography style={headerStyles.headerText} id="APP_NAME" />
+  const [isSearching, setIsSearching] = useState(false);
+  if (isSearching) {
+    return (
+      <HomeStack.Navigator
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerLeft: () => (
+            <Icon
+              name="bars"
+              type="font-awesome"
+              color={theme.black}
+              onPress={() => navigation.toggleDrawer()}
+              iconStyle={headerStyles.iconLeft}
+            />
+          ),
+          headerRight: () => (
+            <Icon
+              name="times"
+              type="font-awesome"
+              color={theme.black}
+              onPress={() => setIsSearching(false)}
+              iconStyle={globalStyles.icon}
+            />
           ),
         }}
-      />
-    </HomeStack.Navigator>
-  );
+      >
+        <HomeStack.Screen
+          name={screens.home}
+          component={HomeScreen}
+          options={{
+            headerTitle: () => (
+              <TextInput
+                placeholder="Search"
+                autoFocus={true}
+                style={headerStyles.searchBar}
+                onChangeText={(text) => console.log(text)}
+              />
+            ),
+          }}
+        />
+      </HomeStack.Navigator>
+    );
+  } else {
+    return (
+      <HomeStack.Navigator
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerLeft: () => (
+            <Icon
+              name="bars"
+              type="font-awesome"
+              color={theme.black}
+              onPress={() => navigation.toggleDrawer()}
+              iconStyle={headerStyles.iconLeft}
+            />
+          ),
+          headerRight: () => (
+            <Icon
+              name="search"
+              type="font-awesome"
+              color={theme.black}
+              onPress={() => setIsSearching(true)}
+              iconStyle={headerStyles.iconRight}
+            />
+          ),
+        }}
+      >
+        <HomeStack.Screen
+          name={screens.home}
+          component={HomeScreen}
+          options={{
+            headerTitle: () => (
+              <Typography style={headerStyles.headerText} id="APP_NAME" />
+            ),
+          }}
+        />
+      </HomeStack.Navigator>
+    );
+  }
 }
 
 const headerStyles = StyleSheet.create({
@@ -56,5 +102,16 @@ const headerStyles = StyleSheet.create({
   },
   iconRight: {
     marginRight: 16,
+  },
+  searchBar: {
+    textAlign: 'center',
+    minWidth: '100%',
+    height: '70%',
+    borderStyle: 'solid',
+    paddingLeft: '5%',
+    paddingRight: '5%',
+    borderRadius: 20,
+    borderColor: '#2C2C2C',
+    borderWidth: 1,
   },
 });
