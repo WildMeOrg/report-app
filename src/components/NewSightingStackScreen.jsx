f02201be6a864b67ab4548991e6741cc2235fa4b
Daniel <danielborg291@gmail.com>
2020-10-27 14:47:32 -0700

    reduced copy and pasted code

--- src/components/NewSightingStackScreen.jsx
+++ src/components/NewSightingStackScreen.jsx
@@ -1,6 +1,5 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
-  StyleSheet,
   Text,
   View,
   TextInput,
@@ -9,7 +8,7 @@ import {
 } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Icon, withTheme } from 'react-native-elements';
+import { Icon } from 'react-native-elements';
 import screens from '../constants/screens';
 import theme from '../constants/theme';
 import globalStyles from '../styles/globalStyles';
@@ -17,20 +16,28 @@ import styles from '../styles/newSightingStyles';
 
 const NewSightingStack = createStackNavigator();
 
-const NewSightingScreen = ({ navigation }) => {
+function NewSightingForm({ navigation }) {
   const [formSection, setFormSection] = useState(0);
 
-  const formSection0 = () => {
-    return(
-      <View style={styles.container}>
-          <View style={styles.progressBar}>
-            <Animated.View style={(styles.innerProgressBar, styles.thirtyThree)} />
-          </View>
-          <KeyboardAwareScrollView
-            resetScrollToCoords={{ x: 0, y: 0 }}
-            style={styles.keyboardView}
-            scrollEnabled={true}
-          >
+  return (
+    <View style={styles.container}>
+      <View style={styles.progressBar}>
+        <Animated.View
+          style={
+            (styles.innerProgressBar,
+            formSection == 0 ? styles.thirtyThree : null,
+            formSection == 1 ? styles.sixtySix : null,
+            formSection == 2 ? styles.oneHundred : null)
+          }
+        />
+      </View>
+      <KeyboardAwareScrollView
+        resetScrollToCoords={{ x: 0, y: 0 }}
+        style={styles.keyboardView}
+        scrollEnabled={true}
+      >
+        {formSection === 0 ? (
+          <React.Fragment>
             <View style={styles.addNew}>
               <TouchableOpacity
                 onPress={() => navigation.navigate(screens.newSightings[0])}
@@ -67,126 +74,98 @@ const NewSightingScreen = ({ navigation }) => {
               multiline={true}
               numberOfLines={5}
             />
-    
+
             <View style={styles.keyboardView} />
-          </KeyboardAwareScrollView>
-    
-          <View style={styles.buttonContainer}>
-            <View style={styles.horizontal}>
+          </React.Fragment>
+        ) : null}
+        {formSection === 1 ? (
+          <View>
+            <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
+              {' '}
+              Status{' '}
+            </Text>
+            <TextInput style={globalStyles.inputFields} autoCorrect={false} />
+            <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
+              {' '}
+              Relationships{' '}
+            </Text>
+            <TextInput style={globalStyles.inputFields} autoCorrect={false} />
+            <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
+              {' '}
+              Match Individual{' '}
+            </Text>
+            <TextInput style={globalStyles.inputFields} autoCorrect={false} />
+          </View>
+        ) : null}
+        {formSection === 2 ? (
+          <View>
+            <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
+              {' '}
+              Photographer name{' '}
+            </Text>
+            <TextInput style={globalStyles.inputFields} autoCorrect={false} />
+            <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
+              {' '}
+              Photographer email{' '}
+            </Text>
+            <TextInput style={globalStyles.inputFields} autoCorrect={false} />
+          </View>
+        ) : null}
+      </KeyboardAwareScrollView>
+      <View style={styles.buttonContainer}>
+        <View style={styles.horizontal}>
+          {formSection === 0 ? (
+            <View>
               <TouchableOpacity>
                 <View style={[styles.button, globalStyles.invisible]}>
                   <Text style={globalStyles.buttonText}>Back</Text>
                 </View>
               </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => setFormSection(1)}
-              >
+              <TouchableOpacity onPress={() => setFormSection(1)}>
                 <View style={(globalStyles.button, styles.button)}>
                   <Text style={globalStyles.buttonText}>Next </Text>
                 </View>
               </TouchableOpacity>
             </View>
-          </View>
+          ) : null}
+          {formSection === 1 ? (
+            <View>
+              <TouchableOpacity onPress={() => setFormSection(0)}>
+                <View style={[styles.button, styles.buttonInactive]}>
+                  <Text style={globalStyles.buttonText}> Back </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setFormSection(2)}>
+                <View style={styles.button}>
+                  <Text style={globalStyles.buttonText}>Next</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          ) : null}
+          {formSection === 2 ? (
+            <View>
+              <TouchableOpacity onPress={() => setFormSection(1)}>
+                <View style={[styles.button, styles.buttonInactive]}>
+                  <Text style={globalStyles.buttonText}>Back</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  setFormSection(0);
+                  navigation.navigate(screens.home);
+                }}
+              >
+                <View style={styles.button}>
+                  <Text style={globalStyles.buttonText}>Upload</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          ) : null}
         </View>
-    );}
-    const formSection1 = () => {
-      return(<View style={styles.container}>
-        <View style={styles.progressBar}>
-          <Animated.View style={[styles.innerProgressBar, styles.sixtySix]} />
-        </View>
-        
-        <KeyboardAwareScrollView
-          style={{ backgroundColor: '#ffffff' }}
-          resetScrollToCoords={{ x: 0, y: 0 }}
-          style={styles.keyboardView}
-          scrollEnabled
-        >
-          <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
-            {' '}
-            Status{' '}
-          </Text>
-          <TextInput style={globalStyles.inputFields} autoCorrect={false} />
-          <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
-            {' '}
-            Relationships{' '}
-          </Text>
-          <TextInput style={globalStyles.inputFields} autoCorrect={false} />
-          <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
-            {' '}
-            Match Individual{' '}
-          </Text>
-          <TextInput style={globalStyles.inputFields} autoCorrect={false} />
-        </KeyboardAwareScrollView>
-        <View style={styles.buttonContainer}>
-          <View style={styles.horizontal}>
-            <TouchableOpacity
-              onPress={() => setFormSection(0)}
-            >
-              <View style={[styles.button, styles.buttonInactive]}>
-                <Text style={globalStyles.buttonText}> Back </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => setFormSection(2)}
-            >
-              <View style={styles.button}>
-                <Text style={globalStyles.buttonText}>Next</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
-        </View>)
-    }
-    const formSection2 = () => {
-      return(
-        <View style={styles.container}>
-        <View style={styles.progressBar}>
-          <Animated.View style={[styles.innerProgressBar, styles.oneHundred]} />
-        </View>
-        <KeyboardAwareScrollView
-          style={({ backgroundColor: '#ffffff' }, styles.keyboardView)}
-          resetScrollToCoords={{ x: 0, y: 0 }}
-          scrollEnabled={true}
-        >
-          <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
-            {' '}
-            Photographer name{' '}
-          </Text>
-          <TextInput style={globalStyles.inputFields} autoCorrect={false} />
-          <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
-            {' '}
-            Photographer email{' '}
-          </Text>
-          <TextInput style={globalStyles.inputFields} autoCorrect={false} />
-        </KeyboardAwareScrollView>
-        <View style={styles.buttonContainer}>
-          <View style={styles.horizontal}>
-            <TouchableOpacity
-              onPress={() => setFormSection(1)}
-            >
-              <View style={[styles.button, styles.buttonInactive]}>
-                <Text style={globalStyles.buttonText}>Back</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => {setFormSection(0); navigation.navigate(screens.home)}}>
-              <View style={styles.button}>
-                <Text style={globalStyles.buttonText}>Upload</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>)
-    }
-
-  switch(formSection) {
-    case 1:
-      return formSection1();
-    case 2:
-      return formSection2();
-    default:
-      return formSection0();
-  }
-};
+      </View>
+    </View>
+  );
+}
 
 export default function NewSightingStackScreen({ navigation }) {
   return (
@@ -198,7 +177,9 @@ export default function NewSightingStackScreen({ navigation }) {
             name="times"
             type="font-awesome"
             color={theme.black}
-            onPress={() => {navigation.navigate(screens.home)}}
+            onPress={() => {
+              navigation.navigate(screens.home);
+            }}
             iconStyle={globalStyles.icon}
           />
         ),
@@ -206,7 +187,7 @@ export default function NewSightingStackScreen({ navigation }) {
     >
       <NewSightingStack.Screen
         name={screens.newSighting}
-        component={NewSightingScreen}
+        component={NewSightingForm}
         options={{
           headerTitle: () => (
             <Text style={globalStyles.headerText}>Sighting Info</Text>
