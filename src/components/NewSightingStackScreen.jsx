228e910aa7b9ea5b144ff896adabbcf5447b6860
Daniel <danielborg291@gmail.com>
2020-10-28 10:14:06 -0700

    initial formik setup

--- src/components/NewSightingStackScreen.jsx
+++ src/components/NewSightingStackScreen.jsx
@@ -9,6 +9,7 @@ import {
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Icon } from 'react-native-elements';
+import { Formik } from 'formik';
 import screens from '../constants/screens';
 import theme from '../constants/theme';
 import globalStyles from '../styles/globalStyles';
@@ -25,137 +26,185 @@ function NewSightingForm({ navigation }) {
         <Animated.View
           style={
             (styles.innerProgressBar,
-            formSection == 0 ? styles.thirtyThree : null,
-            formSection == 1 ? styles.sixtySix : null,
-            formSection == 2 ? styles.oneHundred : null)
+            formSection === 0 ? styles.thirtyThree : null,
+            formSection === 1 ? styles.sixtySix : null,
+            formSection === 2 ? styles.oneHundred : null)
           }
         />
       </View>
-      <KeyboardAwareScrollView
-        resetScrollToCoords={{ x: 0, y: 0 }}
-        style={styles.keyboardView}
-        scrollEnabled={true}
+      <Formik
+        initialValues={{
+          title: '',
+          location: '',
+          sightingContext: '',
+          status: '',
+          relationships: '',
+          matchIndividual: '',
+          photographerName: '',
+          photographerEmail: '',
+        }}
+        onSubmit={(values) => console.log(values)}
       >
-        {formSection === 0 ? (
+        {(formikProps) => {
           <>
-            <View style={styles.addNew}>
-              <TouchableOpacity
-                onPress={() => navigation.navigate(screens.newSighting)}
-              >
-                <Icon
-                  name="cloud-upload"
-                  type="font-awesome"
-                  color={theme.black}
-                  iconStyle={styles.addText}
-                  size={40}
-                />
-                <Text style={[globalStyles.inputHeader, styles.addText]}>
-                  Add Images
-                </Text>
-              </TouchableOpacity>
-            </View>
-            <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
-              Title
-            </Text>
-            <TextInput style={globalStyles.inputFields} autoCorrect={false} />
-            <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
-              Location
-            </Text>
-            <TextInput style={globalStyles.inputFields} autoCorrect={false} />
-            <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
-              Sighting Context
-            </Text>
-            <TextInput
-              style={[globalStyles.inputFields, styles.multiLine]}
-              autoCorrect={false}
-              multiline={true}
-              numberOfLines={5}
-            />
-            <View style={styles.keyboardView} />
-          </>
-        ) : null}
-        {formSection === 1 ? (
-          <>
-            <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
-              Status
-            </Text>
-            <TextInput style={globalStyles.inputFields} autoCorrect={false} />
-            <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
-              Relationships
-            </Text>
-            <TextInput style={globalStyles.inputFields} autoCorrect={false} />
-            <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
-              Match Individual
-            </Text>
-            <TextInput style={globalStyles.inputFields} autoCorrect={false} />
-          </>
-        ) : null}
-        {formSection === 2 ? (
-          <>
-            <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
-              Photographer name
-            </Text>
-            <TextInput style={globalStyles.inputFields} autoCorrect={false} />
-            <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
-              Photographer email
-            </Text>
-            <TextInput style={globalStyles.inputFields} autoCorrect={false} />
-          </>
-        ) : null}
-      </KeyboardAwareScrollView>
-      {formSection === 0 ? (
-        <View style={styles.buttonContainer}>
-          <View style={styles.horizontal}>
-            <TouchableOpacity>
-              <View style={[styles.button, globalStyles.invisible]}>
-                <Text style={globalStyles.buttonText}>Back</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => setFormSection(1)}>
-              <View style={(globalStyles.button, styles.button)}>
-                <Text style={globalStyles.buttonText}>Next </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
-      ) : null}
-      {formSection === 1 ? (
-        <View style={styles.buttonContainer}>
-          <View style={styles.horizontal}>
-            <TouchableOpacity onPress={() => setFormSection(0)}>
-              <View style={[styles.button, styles.buttonInactive]}>
-                <Text style={globalStyles.buttonText}> Back </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => setFormSection(2)}>
-              <View style={styles.button}>
-                <Text style={globalStyles.buttonText}>Next</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
-      ) : null}
-      {formSection === 2 ? (
-        <View style={styles.buttonContainer}>
-          <View style={styles.horizontal}>
-            <TouchableOpacity onPress={() => setFormSection(1)}>
-              <View style={[styles.button, styles.buttonInactive]}>
-                <Text style={globalStyles.buttonText}>Back</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                setFormSection(0);
-                navigation.navigate(screens.home);
-              }}
+            <KeyboardAwareScrollView
+              resetScrollToCoords={{ x: 0, y: 0 }}
+              style={styles.keyboardView}
+              scrollEnabled={true}
             >
-              <View style={styles.button}>
-                <Text style={globalStyles.buttonText}>Upload</Text>
+              {formSection === 0 && (
+                <>
+                  <View style={styles.addNew}>
+                    <TouchableOpacity
+                      onPress={() => navigation.navigate(screens.newSighting)}
+                    >
+                      <Icon
+                        name="cloud-upload"
+                        type="font-awesome"
+                        color={theme.black}
+                        iconStyle={styles.addText}
+                        size={40}
+                      />
+                      <Text style={[globalStyles.inputHeader, styles.addText]}>
+                        Add Images
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                  <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
+                    Title
+                  </Text>
+                  <TextInput
+                    style={globalStyles.inputFields}
+                    autoCorrect={false}
+                    onChangeText={formikProps.handleChange('title')}
+                  />
+                  <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
+                    Location
+                  </Text>
+                  <TextInput
+                    style={globalStyles.inputFields}
+                    autoCorrect={false}
+                    onChangeText={formikProps.handleChange('location')}
+                  />
+                  <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
+                    Sighting Context
+                  </Text>
+                  <TextInput
+                    style={[globalStyles.inputFields, styles.multiLine]}
+                    autoCorrect={false}
+                    multiline={true}
+                    numberOfLines={5}
+                    onChangeText={formikProps.handleChange('sightingContext')}
+                  />
+                  <View style={styles.keyboardView} />
+                </>
+              )}
+              {formSection === 1 && (
+                <>
+                  <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
+                    Status
+                  </Text>
+                  <TextInput
+                    style={globalStyles.inputFields}
+                    autoCorrect={false}
+                    onChangeText={formikProps.handleChange('status')}
+                  />
+                  <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
+                    Relationships
+                  </Text>
+                  <TextInput
+                    style={globalStyles.inputFields}
+                    autoCorrect={false}
+                    onChangeText={formikProps.handleChange('relationships')}
+                  />
+                  <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
+                    Match Individual
+                  </Text>
+                  <TextInput
+                    style={globalStyles.inputFields}
+                    autoCorrect={false}
+                    onChangeText={formikProps.handleChange('matchIndividual')}
+                  />
+                </>
+              )}
+              {formSection === 2 && (
+                <>
+                  <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
+                    Photographer name
+                  </Text>
+                  <TextInput
+                    style={globalStyles.inputFields}
+                    autoCorrect={false}
+                    onChangeText={formikProps.handleChange('photographerName')}
+                  />
+                  <Text style={[globalStyles.h2Text, globalStyles.inputHeader]}>
+                    Photographer email
+                  </Text>
+                  <TextInput
+                    style={globalStyles.inputFields}
+                    autoCorrect={false}
+                    onChangeText={formikProps.handleChange('photographerEmail')}
+                  />
+                </>
+              )}
+            </KeyboardAwareScrollView>
+            {formSection === 0 && (
+              <View style={styles.buttonContainer}>
+                <View style={styles.horizontal}>
+                  <TouchableOpacity>
+                    <View style={[styles.button, globalStyles.invisible]}>
+                      <Text style={globalStyles.buttonText}>Back</Text>
+                    </View>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => setFormSection(1)}>
+                    <View style={(globalStyles.button, styles.button)}>
+                      <Text style={globalStyles.buttonText}>Next </Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
               </View>
-            </TouchableOpacity>
-          </View>
-        </View>
-      ) : null}
+            )}
+            {formSection === 1 && (
+              <View style={styles.buttonContainer}>
+                <View style={styles.horizontal}>
+                  <TouchableOpacity onPress={() => setFormSection(0)}>
+                    <View style={[styles.button, styles.buttonInactive]}>
+                      <Text style={globalStyles.buttonText}> Back </Text>
+                    </View>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => setFormSection(2)}>
+                    <View style={styles.button}>
+                      <Text style={globalStyles.buttonText}>Next</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            )}
+            {formSection === 2 && (
+              <View style={styles.buttonContainer}>
+                <View style={styles.horizontal}>
+                  <TouchableOpacity onPress={() => setFormSection(1)}>
+                    <View style={[styles.button, styles.buttonInactive]}>
+                      <Text style={globalStyles.buttonText}>Back</Text>
+                    </View>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => {
+                      setFormSection(0);
+                      navigation.navigate(screens.home);
+                      formikProps.handleSubmit;
+                    }}
+                  >
+                    <View style={styles.button}>
+                      <Text style={globalStyles.buttonText}>Upload</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            )}
+          </>;
+        }}
+      </Formik>
     </View>
   );
 }
