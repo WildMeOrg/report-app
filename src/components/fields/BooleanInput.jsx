dc06e725cafc003c1acf9c1acd8655f1a564cc3f
pascuad21 <pascuad21@up.edu>
2021-02-01 17:27:15 -0800

    Custome Field changes
    Removed forloop form LocationID
    Changed BooleanInput to toggle

--- src/components/fields/BooleanInput.jsx
+++ src/components/fields/BooleanInput.jsx
@@ -2,24 +2,31 @@ import React, { useState } from 'react';
 import { Picker } from '@react-native-community/picker';
 import globalStyles from '../../styles/globalStyles';
 import styles from '../../styles/newSightingStyles';
-
+import { Switch } from 'react-native-gesture-handler';
+import theme from '../../constants/theme';
 export default function BooleanInput(rest) {
   const { name, schema, props } = rest;
   const [choice, setChoice] = useState(false);
   return (
-    <Picker
-      selectedValue={props.values.customFields[name] || choice}
-      style={{ marginHorizontal: '5%' }}
-      onValueChange={(itemValue) => {
-        [
-          setChoice(itemValue),
-          props.setFieldValue(`customFields.${name}`, itemValue),
-        ];
-      }}
-      onBlur={props.handleBlur(`customFields.${name}`)}
-    >
-      <Picker.Item label={'False'} value={false} key={0} />
-      <Picker.Item label={'True'} value={true} key={1} />
-    </Picker>
+    // <Picker
+    //   selectedValue={props.values.customFields[name] || choice}
+    //   style={{ marginHorizontal: '5%' }}
+    //   onValueChange={(itemValue) => {
+    //     [
+    //       setChoice(itemValue),
+    //       props.setFieldValue(`customFields.${name}`, itemValue),
+    //     ];
+    //   }}
+    //   onBlur={props.handleBlur(`customFields.${name}`)}
+    // >
+    //   <Picker.Item label={'False'} value={false} key={0} />
+    //   <Picker.Item label={'True'} value={true} key={1} />
+    // </Picker>
+    <Switch
+      trackColor={{ true: theme.green, false: theme.grey }}
+      thumbColor={theme.white}
+      onValueChange={() => setChoice(!choice)}
+      value={choice}
+    />
   );
 }
