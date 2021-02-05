00f55673150fdc21b4842d29af32825c7bf94ae8
degrood21 <35583946+degrood21@users.noreply.github.com>
2021-02-05 11:23:24 -0800

    Added Validation

--- src/components/fields/BooleanInput.jsx
+++ src/components/fields/BooleanInput.jsx
@@ -7,26 +7,17 @@ import theme from '../../constants/theme';
 export default function BooleanInput(rest) {
   const { name, schema, props } = rest;
   const [choice, setChoice] = useState(false);
+  console.log(props);
   return (
-    // <Picker
-    //   selectedValue={props.values.customFields[name] || choice}
-    //   style={{ marginHorizontal: '5%' }}
-    //   onValueChange={(itemValue) => {
-    //     [
-    //       setChoice(itemValue),
-    //       props.setFieldValue(`customFields.${name}`, itemValue),
-    //     ];
-    //   }}
-    //   onBlur={props.handleBlur(`customFields.${name}`)}
-    // >
-    //   <Picker.Item label={'False'} value={false} key={0} />
-    //   <Picker.Item label={'True'} value={true} key={1} />
-    // </Picker>
     <Switch
       trackColor={{ true: theme.green, false: theme.grey }}
       thumbColor={theme.white}
       style={{ marginHorizontal: '5%' }}
-      onValueChange={() => setChoice(!choice)}
+      onValueChange={() => [
+        setChoice(!choice),
+        props.setFieldValue(`customFields.${name}`, !choice),
+      ]}
+      onBlur={props.handleBlur(`customFields.${name}`)}
       value={choice}
     />
   );
